5fdd44c627487ce7ac9ec0a958ba3c3f586cddf1
Jordan Enev <jordantoen@gmail.com>
2016-04-18 23:30:35 +0300

    Move data files to own folder.

--- polymorphism/php/index.php
+++ polymorphism/php/index.php
@@ -5,14 +5,15 @@ require 'CSV_Articles_Parser.php';
 require 'TXT_Articles_Parser.php';
 require 'Articles_Collection.php';
 
-$current_directory = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+$current_dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
+$data_dir    = $current_dir . 'data' . DIRECTORY_SEPARATOR;
 
-$csv_articles = new CSV_Articles_Parser();
-$csv_articles->file_path =  $current_directory . 'articles.csv';
+$csv_articles            = new CSV_Articles_Parser();
+$csv_articles->file_path = $data_dir . 'articles.csv';
 
-$txt_articles = new TXT_Articles_Parser();
-$txt_articles->file_path =  $current_directory . 'articles.txt';
+$txt_articles            = new TXT_Articles_Parser();
+$txt_articles->file_path = $data_dir . 'articles.txt';
 
-Articles_Collection::render($csv_articles);
-Articles_Collection::render($txt_articles);
+Articles_Collection::render( $csv_articles );
+Articles_Collection::render( $txt_articles );
 
